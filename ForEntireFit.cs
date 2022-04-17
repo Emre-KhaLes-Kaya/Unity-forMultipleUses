d97e249fec009ca6932fcfe0991cf50bc943c5e6
Emre Kaya <102796012+Emre-KhaLes-Kaya@users.noreply.github.com>
2022-04-17 13:22:47 +0300

    Add files via upload

---
+++ ForEntireFit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEntireFit : MonoBehaviour
+{
+    public SpriteRenderer rink;
+
+    // Use this for initialization
+    void Start()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = rink.bounds.size.x / rink.bounds.size.y;
+
+        if (screenRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = rink.bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = rink.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+}
